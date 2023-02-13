a29822f70c6b422915382041fb576adfb0e22254
Yuko Zheng <hi@yuko.me>
2023-02-13 20:57:54 +0800

    Update WebDAVMiddlewareOptions.cs

--- src/Pomelo.Storage.WebDAV/Options/WebDAVMiddlewareOptions.cs
+++ src/Pomelo.Storage.WebDAV/Options/WebDAVMiddlewareOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace Pomelo.Storage.WebDAV.Options
+﻿// Copyright (c) Yuko(Yisheng) Zheng. All rights reserved.
+// Licensed under the MIT. See LICENSE in the project root for license information.
+
+namespace Pomelo.Storage.WebDAV.Options
 {
     public class WebDAVMiddlewareOptions
     {
